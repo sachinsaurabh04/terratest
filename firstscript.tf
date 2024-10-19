fbd4f90f80ffe7c5e7cc188f7b287c43bd5ebf54
Sachin <sachinsaurabh2020@gmail.com>
2024-10-19 23:58:04 +0530

    changed to awscode

--- firstscript.tf
+++ firstscript.tf
@@ -1,4 +1,13 @@
-resource "local_file" "foo" {
-  content  = "This is my first terraform code on centos 8."
-  filename = "${path.module}/testfile"
+provider "aws" {
+  region = "us-east-2"
+  AWS_ACCESS_KEY_ID = "AKIAQLVQQVLYJ5ESV4FN"
+  AWS_SECRET_ACCESS_KEY = "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"
+}
+
+resource "aws_instance" "webserver" {
+  ami           = "ami-050cd642fd83388e4"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "webserver"
+  }
 }
