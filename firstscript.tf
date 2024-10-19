9d823d1d1ebf9e9ecd3c1d8cf27c01486a91dc2f
Sachin <sachinsaurabh2020@gmail.com>
2024-10-20 01:03:42 +0530

    correcting the provider

--- firstscript.tf
+++ firstscript.tf
@@ -1,6 +1,3 @@
-provider "aws" {
-  region = "us-east-2"
-}
 
 resource "aws_instance" "webserver" {
   ami           = "ami-050cd642fd83388e4"
--- provider.tf
+++ provider.tf
@@ -1,4 +1,5 @@
 provider "aws" {
+  region = "us-east-2"
   access_key = "AKIAQLVQQVLYJ5ESV4FN"
   secret_key = "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"
 }
