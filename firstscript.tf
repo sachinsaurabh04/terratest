677a02fd935c3cf0ab58558ec2c656f17a77e2b4
Sachin <sachinsaurabh2020@gmail.com>
2024-10-20 00:15:11 +0530

    removed credentials

--- firstscript.tf
+++ firstscript.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region = "us-east-2"
-  AWS_ACCESS_KEY_ID = "AKIAQLVQQVLYJ5ESV4FN"
-  AWS_SECRET_ACCESS_KEY = "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"
 }
 
 resource "aws_instance" "webserver" {
